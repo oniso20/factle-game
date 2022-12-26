a8d04a77a7992cab337ee4707d0a2619f7bc0c87
Oniso20 <emem.c.onis@gmail.com>
2022-12-26 23:09:46 +0200

    Fix mobile UI

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -4,7 +4,7 @@ const Question = ({ fact }) => {
   return (
     <>
       <div className="question">
-        <h3>The question for today is: {fact && fact.prompt}</h3>
+        <h3>{fact && fact.prompt}</h3>
         <div className="position">
           <div>1st</div>
           <div>2nd</div>
